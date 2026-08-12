772b0b16ee55d124f93ecc34a938b662b30a8b11
ryan <ryanwhytejamaica@gmail.com>
2026-08-12 10:42:57 -0500

    Add project files.

---
+++ Finance App/Controllers/ExpensesController.cs
@@ -0,0 +1,19 @@
+﻿using Finance_App.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance_App.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly FinanceAppContext context;
+        public ExpensesController(FinanceAppContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var expenses = context.Expenses.ToList();
+            return View(expenses);
+        }
+    }
+}
---
+++ Finance App/Data/FinanceAppContext.cs
@@ -0,0 +1,12 @@
+﻿using Finance_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance_App.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options) { }
+
+       public DbSet<Expense> Expenses { get; set; }
+    }
+}
